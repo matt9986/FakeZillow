00552b96f24e40611ad8a80aa301e3fed3f29e0f
Matt Purdy <matt9986@gmail.com>
2013-10-01 18:15:14 +0000

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZillowClone::Application.config.secret_token = '63a325d9fa1326c123a9796016aca6481dc6707479728a793fea58fd85f9489af047b4cc081d9df0c9b4e7c911120170922bbeb2ad4f0d36d271bc638bbc5258'
