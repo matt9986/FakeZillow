44c1282aee8a8dd7be6aa195d779ee37370548a3
Matt Purdy <matt9986@gmail.com>
2013-10-03 20:42:01 +0000

    Added option for additional search params

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,6 +6,7 @@ class SearchesController < ApplicationController
   def index
     lat, lng = params[:listing][:coord].split(", ")
     @listings = Listing.find_near_coord(lat.to_f, lng.to_f).page(params[:page])
+    @listings = @listings.where(price: 0 .. params[:listing][:max_price].to_i) unless params[:listing][:max_price].empty?
     
     @json = @listings.to_gmaps4rails do |listing, marker|
       marker.infowindow render_to_string(:partial => "/listings/map_window", :locals => { :listing => listing})
